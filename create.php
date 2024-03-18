7f2b8d08c70c91fac8e412af861ccdf4cdcfbcb6
AAA-Foundation <163861742+AAA-Foundation@users.noreply.github.com>
2024-03-18 18:36:34 +0530

    Add files via upload

---
+++ create.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+
+    <body>
+        <h3><p class="p1">CREATE ACCOUNT </p></h3>
+  
+        <a> ENTER<input type="email" name="email"  class ="a" placeholder="Enter email id" required></a><br>
+        <input type="password" name="password" class ="b" placeholder="Password" required><br>
+        <input type="submit" name="login" value=" Create " class ="p2">
+      
+    </body>
+    </head>
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <link rel= "stylesheet" href ="assests/css/style.css">
+    <body>
+        <h1><p class="p1">LOGIN PAGE</p></h1>
+    <form method="post" action="Control/login.php">
+        <input type="email" name="email"  class ="a" placeholder="Enter email id" required><br>
+        <input type="password" name="password" class ="b" placeholder="Password" required><br>
+        <button class ="button-85" role="button">SIGN IN</button>
+      
+    </form>
+      
+    </body>
+    <body>
+    <a href= "create.php"><input type="submit" name="login" value="sign up " class ="p3" ></a>
+    </body>
+
+    </head>
+</html>
